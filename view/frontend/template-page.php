485bd18950bcc43daed98c01233ac5cf037f9c02
Robin Ponçon <poncon.robin@gmail.com>
2019-12-18 10:38:57 +0100

    Ajout du template de page

---
+++ view/frontend/template-page.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title><?= $title ?></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../css/projet-4.css">
+</head>
+<body>
+	<?= $content ?>
+</body>
+</html>
