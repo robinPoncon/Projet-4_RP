2e3acd399085cb2b975d28df9d89138d5a13f728
Robin Ponçon <poncon.robin@gmail.com>
2020-01-03 15:51:25 +0100

    Ajout de commentaire et documentation pour la classe Post

--- classes/Post.class.php
+++ classes/Post.class.php
@@ -4,12 +4,11 @@ namespace RobinP\classes;
 use \RobinP\classes\Entity;
 use \DateTime;
 
-/*function validateDate($date, $format = 'Y-m-d H:i:s')
-	{
-    	$d = DateTime::createFromFormat($format, $date);
-    	return $d && $d->format($format) == $date;
-	}
+/**
+* La classe Post permet de créer un objet Post(article) qui va récupérer en entrée les données de BDD
+* @Author Robin Ponçon
 */
+
 class Post extends Entity
 {
 	private $id;
@@ -18,27 +17,52 @@ class Post extends Entity
 	private $author;
 	private $creation_date;
 
-	// Les getters
+					// Les getters
+
+	/**
+	* Permet de récupérer l'id d'un article
+	* @return Int $id : Retourne l'id de l'article
+	*/
 
 	public function getId()
 	{
 		return $this->id;
 	}
 
+	/**
+	* Permet de récupérer le titre d'un article
+	* @return String $title : Retourne le titre de l'article
+	*/
+
 	public function getTitle()
 	{
 		return $this->title;
 	}
+
+	/**
+	* Permet de récupérer le contenu d'un article
+	* @return String $content : Retourne le contenu de l'article
+	*/
 
 	public function getContent()
 	{
 		return $this->content;
 	}
 
+	/**
+	* Permet de récupérer l'auteur d'un article
+	* @return String $author : Retourne l'auteur de l'article
+	*/
+
 	public function getAuthor()
 	{
 		return $this->author;
 	}
+
+	/**
+	* Permet de récupérer la date d'un article
+	* @return timestamp $creation_date : Retourne la date de l'article
+	*/
 
 	public function getCreationDate()
 	{
@@ -46,7 +70,13 @@ class Post extends Entity
 	}
 
 
-	// Les setters
+					// Les setters
+
+	/**
+	* Permet de définir la valeur de l'id qu'on récupère via la BDD si il est supérieur à 0.
+	* @param int $id : id venant de la BDD
+	* @return int $id : on récupère l'id que l'on affecte à notre propriété de classe $id;
+	*/
 
 	public function setId($id)
 	{
@@ -58,6 +88,12 @@ class Post extends Entity
 		}
 	}
 
+	/**
+	* Permet de définir la valeur du titre qu'on récupère via la BDD si il est de type string.
+	* @param string $title : titre venant de la BDD
+	* @return string $title : on récupère le titre que l'on affecte à notre propriété de classe $title;
+	*/
+
 	public function setTitle($title)
 	{
 		if(is_string($title))
@@ -66,6 +102,12 @@ class Post extends Entity
 		}
 	}
 
+	/**
+	* Permet de définir la valeur du contenu qu'on récupère via la BDD si il est de type string.
+	* @param string $content : contenu venant de la BDD
+	* @return string $content : on récupère le contenu que l'on affecte à notre propriété de classe $contenu;
+	*/
+
 	public function setContent($content)
 	{
 		if(is_string($content))
@@ -73,6 +115,12 @@ class Post extends Entity
 			return $this->content = $content;
 		}
 	}
+
+	/**
+	* Permet de définir la valeur de l'auteur qu'on récupère via la BDD si il est de type string.
+	* @param string $author : auteur venant de la BDD
+	* @return string $author : on récupère l'auteur que l'on affecte à notre propriété de classe $author;
+	*/
 
 	public function setAuthor($author)
 	{
@@ -84,19 +132,7 @@ class Post extends Entity
 
 	public function setCreationDate($creation_date)
 	{
-		/*if (validateDate($creation_date) === false)
-		{
-  			echo "test";
-		}
-
-		else
-		{
-			return $this->creation_date = $creation_date;
-		}
-		*/
-
 		return $this->creation_date = $creation_date;
-
 	}
 
 
@@ -106,9 +142,3 @@ class Post extends Entity
 	}
 		
 }
-
-
-
-
-
-?>
