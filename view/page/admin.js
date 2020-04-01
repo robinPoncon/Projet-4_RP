16b5b7ceb58462af9232068c8c6b3aa32b4b56c4
Robin Ponçon <poncon.robin@gmail.com>
2020-04-01 11:07:04 +0200

    Commentaire page JS

--- view/page/admin.js
+++ view/page/admin.js
@@ -1,18 +1,5 @@
 
-$("#modifMDP").click(function(){
-
-	if ($("#changeMDP input").attr("class") === "d-none")
-	{
-		$("#changeMDP input").attr("class", "d-block form-control");
-		$("#changePseudo input").attr("class", "d-none");
-		$("#changeEmail input").attr("class", "d-none");
-	}
-
-	else
-	{
-		$("#changeMDP input").attr("class", "d-none");
-	}
-});
+// Fonction qui affiche les inputs pour modifier le pseudo et cache les inputs des autres
 
 $("#modifPseudo").click(function(){
 
@@ -28,6 +15,25 @@ $("#modifPseudo").click(function(){
 		$("#changePseudo input").attr("class", "d-none");
 	}
 });
+
+// Fonction qui affiche les inputs pour modifier le mot de passe et cache les inputs des autres 
+
+$("#modifMDP").click(function(){
+
+	if ($("#changeMDP input").attr("class") === "d-none")
+	{
+		$("#changeMDP input").attr("class", "d-block form-control");
+		$("#changePseudo input").attr("class", "d-none");
+		$("#changeEmail input").attr("class", "d-none");
+	}
+
+	else
+	{
+		$("#changeMDP input").attr("class", "d-none");
+	}
+});
+
+// Fonction qui affiche les inputs pour modifier l'émail et cache les inputs des autres
 
 $("#modifEmail").click(function(){
 
@@ -45,30 +51,15 @@ $("#modifEmail").click(function(){
 
 });
 
-$(".buttonUpdate").click(function(){
-
-	if ($(".mytextarea").css("display") === "none")
-	{
-		$(".mytextarea").css("display", "inline-block");
-	}
-	else
-	{
-		$(".mytextarea").css("display", "none");
-	}
-});
+// Paramètre pour l'éditeur HTML
 
 tinymce.init({
     selector: '#mytextarea',
     height: "400px"
 });
 
-/*function validerSuppression(link){
-    if(confirm('Confirmer la suppression ?')){
-     document.location.href = link;
-    }
-} */
 
-
+// Fonction qui affiche la div confirm et réduit l'opacité du bloc news ainsi que la désactivation des events pour plus de lisibilité
 
 function deletePost(id)
 {
@@ -76,11 +67,23 @@ function deletePost(id)
 	$(".news").css("opacity", 0.5).css("pointer-events", "none");
 }
 
+// Fonction qui recache la div confirm et remet l'opacité du bloc news ainsi que les events normalement 
+
+function cancelPost(id)
+{
+	$("#confirm" + id).css("visibility", "hidden");
+	$(".news").css("opacity", 1).css("pointer-events", "auto");
+}
+
+// Fonction qui affiche la div confirm et réduit l'opacité de la page monCompte ainsi que la désactivation des events pour plus de lisibilité
+
 function deleteComment(id)
 {
 	$("#confirm" + id).css("visibility", "visible");
 	$(".commentaire, #perso, #newPost, .commentaireSignaler, h4").css("opacity", 0.5).css("pointer-events", "none");
 }
+
+// Fonction qui recache la div confirm et remet l'opacité de la page monCompte ainsi que les events normalement 
 
 function cancelComment(id)
 {
@@ -88,11 +91,6 @@ function cancelComment(id)
 	$(".commentaire, #perso, #newPost, .commentaireSignaler, h4").css("opacity", 1).css("pointer-events", "auto");
 }
 
-function cancelPost(id)
-{
-	$("#confirm" + id).css("visibility", "hidden");
-	$(".news").css("opacity", 1).css("pointer-events", "auto");
-}
 
 
 
